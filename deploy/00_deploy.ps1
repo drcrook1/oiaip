7e153b186e3c3d3594d68e9b47cd507fe12b257f
David Crook <dacrook@microsoft.com>
2019-11-08 18:01:22 -0500

    pushing for access from other machine to finish writing

--- deploy/00_deploy.ps1
+++ deploy/00_deploy.ps1
@@ -4,6 +4,7 @@ $location = "eastus"
 
 Login-AzureRmAccount
 Select-AzureRmSubscription -SubscriptionName "jayoung-Team Subscription"
+az account set -s "jayoung-Team Subscription"
 
 #Create or check for existing resource group
 $resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
@@ -16,14 +17,23 @@ else{
     Write-Host "Using existing resource group '$resourceGroupName'";
 }
 
-az ad sp create-for-rbac -n "dacrookoiaip"
+$sp_rbac = az ad sp create-for-rbac -n "dacrookoiaip" | ConvertFrom-Json
+ssh-keygen -t rsa -b 2048 -f ./rsa.txt -N mypassword
+$pub_key = Get-Content ./rsa.txt.pub 
+
+Write-Host "Key is: '$pub_key' "
 
 $templateParamObject = @{
     "baseName" = "dacrook"
+    "dnsPrefix" = "oiaip"
+    "servicePrincipalClientId" = ([string]$sp_rbac.appId | ConvertTo-SecureString -AsPlainText -Force)
+    "servicePrincipalClientSecret" = ([string]$sp_rbac.password | ConvertTo-SecureString -AsPlainText -Force)
+    "sshRSAPublicKey" = $pub_key
+    "linuxAdminUserName" = "david"
 }
 
 #Start Actual Deployment
 New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName `
                                     -Mode Incremental `
-                                    -TemplateFile "./arm_templates/00_ml_template.json" `
+                                    -TemplateFile "./arm_templates/00_template.json" `
                                     -TemplateParameterObject $templateParamObject
